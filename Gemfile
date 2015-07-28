310d31683133b3d784c8ca42a4a2be192e0217aa
Sudhir Jonathan <sudhirjonathan@Air.local>
2015-07-28 19:51:29 +0530

    Added skeleton

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+gem 'rails', '4.2.3'
+gem 'rails-api'
+gem 'pg'
+gem 'unicorn'
+
+gem 'spring', :group => :development
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Notecase::Application.config.secret_token = '4f6ed9f97a97045650b2c218300211a5989681369d026e611017c215bff49d7c8515fd75d786d497dc32068f5cb6b8841e3f911d6340495dade5faef40c49e23'
