bb93a108a726094b0f0094d08c22e89397b748ad
Sudhir Jonathan <sudhir.j@gmail.com>
2015-07-28 20:11:53 +0530

    Added double entry

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ gem 'rails', '4.2.3'
 gem 'rails-api'
 gem 'pg'
 gem 'unicorn'
+gem 'double_entry'
 
 gem 'spring', :group => :development
--- db/structure.sql
+++ db/structure.sql
@@ -63,6 +63,154 @@ ALTER SEQUENCE accounts_id_seq OWNED BY accounts.id;
 
 
 --
+-- Name: double_entry_account_balances; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE double_entry_account_balances (
+    id integer NOT NULL,
+    account character varying(31) NOT NULL,
+    scope character varying(23),
+    balance integer NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: double_entry_account_balances_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE double_entry_account_balances_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: double_entry_account_balances_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE double_entry_account_balances_id_seq OWNED BY double_entry_account_balances.id;
+
+
+--
+-- Name: double_entry_line_aggregates; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE double_entry_line_aggregates (
+    id integer NOT NULL,
+    function character varying(15) NOT NULL,
+    account character varying(31) NOT NULL,
+    code character varying(47),
+    scope character varying(23),
+    year integer,
+    month integer,
+    week integer,
+    day integer,
+    hour integer,
+    amount integer NOT NULL,
+    filter character varying,
+    range_type character varying(15) NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: double_entry_line_aggregates_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE double_entry_line_aggregates_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: double_entry_line_aggregates_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE double_entry_line_aggregates_id_seq OWNED BY double_entry_line_aggregates.id;
+
+
+--
+-- Name: double_entry_line_checks; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE double_entry_line_checks (
+    id integer NOT NULL,
+    last_line_id integer NOT NULL,
+    errors_found boolean NOT NULL,
+    log text,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: double_entry_line_checks_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE double_entry_line_checks_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: double_entry_line_checks_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE double_entry_line_checks_id_seq OWNED BY double_entry_line_checks.id;
+
+
+--
+-- Name: double_entry_lines; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE double_entry_lines (
+    id integer NOT NULL,
+    account character varying(31) NOT NULL,
+    scope character varying(23),
+    code character varying(47) NOT NULL,
+    amount integer NOT NULL,
+    balance integer NOT NULL,
+    partner_id integer,
+    partner_account character varying(31) NOT NULL,
+    partner_scope character varying(23),
+    detail_id integer,
+    detail_type character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL
+);
+
+
+--
+-- Name: double_entry_lines_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE double_entry_lines_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: double_entry_lines_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE double_entry_lines_id_seq OWNED BY double_entry_lines.id;
+
+
+--
 -- Name: schema_migrations; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -79,6 +227,34 @@ ALTER TABLE ONLY accounts ALTER COLUMN id SET DEFAULT nextval('accounts_id_seq':
 
 
 --
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY double_entry_account_balances ALTER COLUMN id SET DEFAULT nextval('double_entry_account_balances_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY double_entry_line_aggregates ALTER COLUMN id SET DEFAULT nextval('double_entry_line_aggregates_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY double_entry_line_checks ALTER COLUMN id SET DEFAULT nextval('double_entry_line_checks_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY double_entry_lines ALTER COLUMN id SET DEFAULT nextval('double_entry_lines_id_seq'::regclass);
+
+
+--
 -- Name: accounts_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -87,10 +263,91 @@ ALTER TABLE ONLY accounts
 
 
 --
+-- Name: double_entry_account_balances_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY double_entry_account_balances
+    ADD CONSTRAINT double_entry_account_balances_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: double_entry_line_aggregates_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY double_entry_line_aggregates
+    ADD CONSTRAINT double_entry_line_aggregates_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: double_entry_line_checks_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY double_entry_line_checks
+    ADD CONSTRAINT double_entry_line_checks_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: double_entry_lines_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY double_entry_lines
+    ADD CONSTRAINT double_entry_lines_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: index_account_balances_on_account; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX index_account_balances_on_account ON double_entry_account_balances USING btree (account);
+
+
+--
+-- Name: index_account_balances_on_scope_and_account; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE UNIQUE INDEX index_account_balances_on_scope_and_account ON double_entry_account_balances USING btree (scope, account);
+
+
+--
 -- Name: index_accounts_on_ref; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE UNIQUE INDEX index_accounts_on_ref ON accounts USING btree (ref);
+
+
+--
+-- Name: line_aggregate_idx; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX line_aggregate_idx ON double_entry_line_aggregates USING btree (function, account, code, year, month, week, day);
+
+
+--
+-- Name: lines_account_code_created_at_idx; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX lines_account_code_created_at_idx ON double_entry_lines USING btree (account, code, created_at);
+
+
+--
+-- Name: lines_account_created_at_idx; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX lines_account_created_at_idx ON double_entry_lines USING btree (account, created_at);
+
+
+--
+-- Name: lines_scope_account_created_at_idx; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX lines_scope_account_created_at_idx ON double_entry_lines USING btree (scope, account, created_at);
+
+
+--
+-- Name: lines_scope_account_id_idx; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX lines_scope_account_id_idx ON double_entry_lines USING btree (scope, account, id);
 
 
 --
@@ -107,4 +364,6 @@ CREATE UNIQUE INDEX unique_schema_migrations ON schema_migrations USING btree (v
 SET search_path TO "$user",public;
 
 INSERT INTO schema_migrations (version) VALUES ('20150728143130');
+
+INSERT INTO schema_migrations (version) VALUES ('20150728143914');
 
