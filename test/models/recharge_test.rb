2330fbaba5272266360d539bd327d68bd74de117
Sudhir Jonathan <sudhir.j@gmail.com>
2015-08-03 21:49:01 +0530

    Added tests for spend as well

--- test/models/recharge_test.rb
+++ test/models/recharge_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class RechargeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "recharges" do
+    card = Recharger.where(ref: 'card').first_or_create
+    assert_equal 0, card.balance
+    wallet1 = Wallet.where(ref: 'user1').first_or_create
+    assert_equal 0, wallet1.balance
+
+    card.recharge(wallet1, 100_00, 'r1', {})
+
+    assert_equal -100_00, card.balance
+    assert_equal 100_00, wallet1.balance
+
+    wallet2 = Wallet.where(ref: 'user2').first_or_create
+    card.recharge(wallet2, 100_00, 'r2', {})
+
+    assert_equal -200_00, card.balance
+    assert_equal 100_00, wallet1.balance
+    assert_equal 100_00, wallet2.balance
+
+    assert_equal 0, [card, wallet1, wallet2].map(&:balance).sum
+  end
+
+  test "idempotency" do
+    card = Recharger.where(ref: 'card').first_or_create
+    wallet1 = Wallet.where(ref: 'user1').first_or_create
+    assert_equal 0, wallet1.balance
+
+    card.recharge(wallet1, 100_00, 'r1', {})
+    card.recharge(wallet1, 100_00, 'r1', {})
+    card.recharge(wallet1, 100_00, 'r1', {})
+
+    assert_equal -100_00, card.balance
+    assert_equal 100_00, wallet1.balance
+    assert_equal 1, Transaction.count
+
+    wallet2 = Wallet.where(ref: 'user2').first_or_create
+    card.recharge(wallet2, 100_00, 'r1', {})
+    card.recharge(wallet2, 100_00, 'r1', {})
+    card.recharge(wallet2, 100_00, 'r1', {})
+
+    assert_equal -100_00, card.balance
+    assert_equal 100_00, wallet1.balance
+    assert_equal 0, wallet2.balance
+    assert_equal 1, Transaction.count
+
+    assert_equal 0, [card, wallet1, wallet2].map(&:balance).sum
+  end
 end
--- test/models/spend_test.rb
+++ test/models/spend_test.rb
@@ -1,7 +1,56 @@
 require 'test_helper'
 
 class SpendTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "spends" do
+    card = Recharger.where(ref: 'card').first_or_create
+    wallet1 = Wallet.where(ref: 'user1').first_or_create
+    card.recharge(wallet1, 100_00, 'r1', {})
+    assert_equal 100_00, wallet1.balance
+
+    food = Revenue.where(ref: 'food').first_or_create
+    assert_equal 0, food.balance
+
+    wallet1.spend(food, 50_00, 'samosa', {})
+    assert_equal 50_00, food.balance
+    assert_equal 50_00, wallet1.balance
+
+    tickets = Revenue.where(ref: 'tickets').first_or_create
+    assert_equal 0, tickets.balance
+
+    wallet1.spend(tickets, 40_00, 'movie', {})
+    assert_equal 40_00, tickets.balance
+    assert_equal 50_00, food.balance
+    assert_equal 10_00, wallet1.balance
+
+    assert_raises(DoubleEntry::AccountWouldBeSentNegative) do
+      wallet1.spend(tickets, 40_00, 'movie2', {})
+    end
+
+    assert_equal 0, [card, wallet1, food, tickets].map(&:balance).sum
+  end
+
+  test "idempotency" do
+    card = Recharger.where(ref: 'card').first_or_create
+    wallet1 = Wallet.where(ref: 'user1').first_or_create
+    card.recharge(wallet1, 100_00, 'r1', {})
+    assert_equal 100_00, wallet1.balance
+
+    food = Revenue.where(ref: 'food').first_or_create
+    assert_equal 0, food.balance
+
+    10.times { wallet1.spend(food, 50_00, 'samosa', {}) }
+    assert_equal 50_00, food.balance
+    assert_equal 50_00, wallet1.balance
+
+    tickets = Revenue.where(ref: 'tickets').first_or_create
+    assert_equal 0, tickets.balance
+
+    wallet1.spend(tickets, 40_00, 'samosa', {})
+    # No new spend will be registered. This ref is already being used.
+    assert_equal 0, tickets.balance
+    assert_equal 50_00, food.balance
+    assert_equal 50_00, wallet1.balance
+
+    assert_equal 0, [card, wallet1, food, tickets].map(&:balance).sum
+  end
 end
