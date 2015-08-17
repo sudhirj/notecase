9ff1c24a695cb5753144642c8e5d739df50a2d6c
Sudhir Jonathan <sudhir.j@gmail.com>
2015-08-17 15:34:27 +0530

    Added Spend API

--- app/controllers/spends_controller.rb
+++ app/controllers/spends_controller.rb
@@ -1,2 +1,8 @@
 class SpendsController < ApplicationController
+  def create
+    wallet = Wallet.where(ref: params[:wallet]).first_or_create!
+    revenue = Revenue.where(ref: params[:revenue]).first_or_create!
+    wallet.spend revenue, params[:amount], params[:ref], params[:data]
+    render json: {}
+  end
 end
--- test/controllers/spends_controller_test.rb
+++ test/controllers/spends_controller_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class SpendsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "spends" do
+    transaction_ref = SecureRandom.uuid
+    w1 = Wallet.where(ref: "wallet1").first_or_create!
+    r1 = Revenue.where(ref: 'food').first_or_create!
+
+    post :create, {
+      ref: "ABCD1234",
+      wallet: w1.ref,
+      revenue: r1.ref,
+      amount: 4200,
+      data: {
+        "k1": "v1"
+      }
+    }
+
+    assert_equal 42_00, r1.balance
+    assert_equal -42_00, w1.balance
+    assert_equal 0, [w1, r1].map(&:balance).sum
+
+    3.times do
+      post :create, {
+        ref: "ABCD1234",
+        wallet: w1.ref,
+        revenue: r1.ref,
+        amount: 4200,
+        data: {
+          "k1": "v1"
+        }
+      }
+    end
+
+    assert_equal 42_00, r1.balance
+    assert_equal -42_00, w1.balance
+    assert_equal 0, [w1, r1].map(&:balance).sum
+  end
 end
