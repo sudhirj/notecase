2bf4c67b730e95c963200189e836db7a85ae3ca0
Sudhir Jonathan <sudhir.j@gmail.com>
2015-08-17 16:13:59 +0530

    Added refund API

--- app/controllers/refunds_controller.rb
+++ app/controllers/refunds_controller.rb
@@ -1,2 +1,6 @@
 class RefundsController < ApplicationController
+  def create
+    Spend.where(ref: params[:ref]).first.refund
+    render json: {}
+  end
 end
--- test/controllers/refunds_controller_test.rb
+++ test/controllers/refunds_controller_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class RefundsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "refunds" do
+    transaction_ref = SecureRandom.uuid
+    w1 = Wallet.where(ref: "wallet1").first_or_create!
+    r1 = Revenue.where(ref: 'food').first_or_create!
+
+    w1.spend(r1, 42_00, 'ABCD1234', {})
+
+    assert_equal 42_00, r1.balance
+    assert_equal -42_00, w1.balance
+    assert_equal 0, [w1, r1].map(&:balance).sum
+
+    post :create, {
+      ref: "ABCD1234",
+      data: {
+        "k1": "v1"
+      }
+    }
+
+    assert_equal 0, r1.balance
+    assert_equal 0, w1.balance
+
+    3.times do
+      post :create, {
+        ref: "ABCD1234",
+        data: {
+          "k1": "v1"
+        }
+      }
+    end
+
+    assert_equal 0, r1.balance
+    assert_equal 0, w1.balance
+  end
 end
