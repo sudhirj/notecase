a38665582ebb7b8124e0c507ba36cccc3b6472e9
FenixAra <aravind_dhan@yahoo.com>
2016-03-15 13:17:34 +0530

    Allowing overdraft if the OVERDRAFT env variable is set to true or 1

--- config/initializers/double_entry.rb
+++ config/initializers/double_entry.rb
@@ -4,7 +4,11 @@ Money.default_currency = Money::Currency.new(ENV["DEFAULT_CURRENCY"] || "INR")
 
 DoubleEntry.configure do |config|
   config.define_accounts do |accounts|
-    accounts.define(identifier: :wallet, scope_identifier: accounts.active_record_scope_identifier(Wallet), positive_only: true)
+    positive_values_only = true
+    if ENV["OVERDRAFT"] == "true" || ENV["OVERDRAFT"] == "1" 
+      positive_values_only = false
+    end
+    accounts.define(identifier: :wallet, scope_identifier: accounts.active_record_scope_identifier(Wallet), positive_only: positive_values_only)
     accounts.define(identifier: :recharger, scope_identifier: accounts.active_record_scope_identifier(Recharger))
     accounts.define(identifier: :revenue, scope_identifier: accounts.active_record_scope_identifier(Revenue), positive_only: true)
   end
