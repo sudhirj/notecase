01b25edf65a855365ceb2535ab1f71fa19e4db28
FenixAra <aravind_dhan@yahoo.com>
2016-03-16 16:09:04 +0530

    Checking for 'yes' as well for overdraft env. Making the overdraft env case insensitive. Renaming positive_values_only to wallet_positive_values_only

--- config/initializers/double_entry.rb
+++ config/initializers/double_entry.rb
@@ -4,11 +4,12 @@ Money.default_currency = Money::Currency.new(ENV["DEFAULT_CURRENCY"] || "INR")
 
 DoubleEntry.configure do |config|
   config.define_accounts do |accounts|
-    positive_values_only = true
-    if ENV["OVERDRAFT"] == "true" || ENV["OVERDRAFT"] == "1" 
-      positive_values_only = false
+    wallet_positive_values_only = true
+    overdraft = ENV["OVERDRAFT"].to_s.downcase
+    if overdraft == "true" || overdraft == "1" || overdraft == "yes"
+      wallet_positive_values_only = false
     end
-    accounts.define(identifier: :wallet, scope_identifier: accounts.active_record_scope_identifier(Wallet), positive_only: positive_values_only)
+    accounts.define(identifier: :wallet, scope_identifier: accounts.active_record_scope_identifier(Wallet), positive_only: wallet_positive_values_only)
     accounts.define(identifier: :recharger, scope_identifier: accounts.active_record_scope_identifier(Recharger))
     accounts.define(identifier: :revenue, scope_identifier: accounts.active_record_scope_identifier(Revenue), positive_only: true)
   end
