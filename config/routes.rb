e95e4a9f24ebd15b96d919ebe87bcc4407257846
Sudhir Jonathan <sudhir.j@gmail.com>
2017-02-23 11:18:18 +0530

    Added mount path configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :rechargers, except: [:new, :edit, :update, :destroy]
-  resources :wallets, except: [:new, :edit, :update, :destroy]
-  resources :revenues, except: [:new, :edit, :update, :destroy]
+  scope ENV.fetch('MOUNT_PATH', '') do
+    resources :rechargers, except: [:new, :edit, :update, :destroy]
+    resources :wallets, except: [:new, :edit, :update, :destroy]
+    resources :revenues, except: [:new, :edit, :update, :destroy]
 
-  resources :recharges, except: [:new, :edit, :update, :destroy]
-  resources :spends, except: [:new, :edit, :update, :destroy]
-  resources :refunds, except: [:new, :edit, :update, :destroy]
+    resources :recharges, except: [:new, :edit, :update, :destroy]
+    resources :spends, except: [:new, :edit, :update, :destroy]
+    resources :refunds, except: [:new, :edit, :update, :destroy]
 
-  get '/', to: 'home#index'
+    get '/', to: 'home#index'
+  end
+
 end
